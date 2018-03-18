262ab7408cd3114c7e02d12bc8521f7b6e1213d6
Eduardo A.F <eacevedof@yahoo.es>
2018-03-18 13:10:50 +0100

    //\app\config\parameters.php 1.0.0

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,26 +1,26 @@
 <?php 
-//\app\config\parameters.php 1.0.0
+//::file:: \app\config\parameters.php 1.0.0
 return array (
-  'parameters' => 
-  array (
-    'database_host' => '127.0.0.1',
-    'database_port' => '',
-    'database_name' => 'db_prestashop',
-    'database_user' => 'root',
-    'database_password' => '',
-    'database_prefix' => 'ps_',
-    'database_engine' => 'InnoDB',
-    'mailer_transport' => 'smtp',
-    'mailer_host' => '127.0.0.1',
-    'mailer_user' => NULL,
-    'mailer_password' => NULL,
-    'secret' => 'D7F8LwkiEFSSqLpoVWLADSqLzzMvPcM4EDurg8EqhKkqxmpnrNLqhL7x',
-    'ps_caching' => 'CacheMemcache',
-    'ps_cache_enable' => false,
-    'ps_creation_date' => '2018-03-18',
-    'locale' => 'es-ES',
-    'cookie_key' => 'b5WHSikFZNgEtv9gbCw0Gp9QO9XO8b14t4sp7myK7w7v4inwroxonTUw',
-    'cookie_iv' => '5dopEA59',
-    'new_cookie_key' => 'def0000093ab26faa5559687ed62d765b6402857aae7da639b83948562a81c5e714fc1baf6b373c37d8f8f596255a3c1115ab380dbb3047af33b82d38c3244169bc21b7a',
-  ),
+    'parameters' =>
+    array (
+        'database_host' => '127.0.0.1',
+        'database_port' => '',
+        'database_name' => 'db_prestashop',
+        'database_user' => 'root',
+        'database_password' => '',
+        'database_prefix' => 'ps_',     
+        'database_engine' => 'InnoDB',
+        'mailer_transport' => 'smtp',
+        'mailer_host' => '127.0.0.1',
+        'mailer_user' => NULL,
+        'mailer_password' => NULL,
+        'secret' => 'D7F8LwkiEFSSqLpoVWLADSqLzzMvPcM4EDurg8EqhKkqxmpnrNLqhL7x',
+        'ps_caching' => 'CacheMemcache',
+        'ps_cache_enable' => false,
+        'ps_creation_date' => '2018-03-18',
+        'locale' => 'es-ES',
+        'cookie_key' => 'b5WHSikFZNgEtv9gbCw0Gp9QO9XO8b14t4sp7myK7w7v4inwroxonTUw',
+        'cookie_iv' => '5dopEA59',
+        'new_cookie_key' => 'def0000093ab26faa5559687ed62d765b6402857aae7da639b83948562a81c5e714fc1baf6b373c37d8f8f596255a3c1115ab380dbb3047af33b82d38c3244169bc21b7a',
+    ),
 );
