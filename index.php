9412a982d148fa72c40ab76187bc5c9125d64e6a
Eduardo A.F <eacevedof@yahoo.es>
2018-03-18 19:53:59 +0100

    ::file:: \index.php 1.0.0

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//::file:: \index.php 1.0.0
 /**
  * 2007-2017 PrestaShop
  *
@@ -23,6 +24,35 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
+error_reporting(0); 
+$old_error_handler = set_error_handler("userErrorHandler"); 
+
+function userErrorHandler($errno,$errmsg,$filename,$linenum,$vars) 
+{ 
+    //https://ayuda.1and1.es/hosting-c85122/scripting-y-programacion-c64780/php-c64788/logs-de-error-de-php-a694887.html
+    $time=date("Ymd H:i:s");
+    // Get the error type from the error number 
+    $errortype = array (1 => "Error", 
+    2 => "Warning", 
+    4 => "Parsing Error", 
+    8 => "Notice", 
+    16 => "Core Error", 
+    32 => "Core Warning", 
+    64 => "Compile Error", 
+    128 => "Compile Warning", 
+    256 => "User Error", 
+    512 => "User Warning", 
+    1024 => "User Notice"); 
+    $errlevel=$errortype[$errno]; 
+
+    //Write error to log file (CSV format) 
+    $errfile=fopen("errors.csv","a"); 
+    fputs($errfile,"\"$time\",\"$filename:$linenum\",\"($errlevel) $errmsg\"\r\n"); 
+    fclose($errfile);
+
+    if($errno!=2 && $errno!=8)
+        die("A fatal error has occurred. Script execution has been aborted"); 
+}//userErrorHandler
 
 require(dirname(__FILE__).'/config/config.inc.php');
 Dispatcher::getInstance()->dispatch();
